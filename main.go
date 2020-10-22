fe0ec824d875760f360884565f724e179d874bc8
cannblw <edgar.95.12@gmail.com>
2020-10-22 23:56:58 +0200

    Add simple usage of scrapper

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	err, content := GetNewsItemContent("https://www.nytimes.com/2020/09/26/opinion/sunday/trump-cuomo-new-york-revenge.html")
+	if err != nil {
+		fmt.Println("Error scrapping news item content")
+	}
+
+	fmt.Println(content)
+}
