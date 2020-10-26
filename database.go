c6690eece312500d32736ccbf80b4591b86212b2
cannblw <edgar.95.12@gmail.com>
2020-10-26 22:56:30 +0100

    Initialize DynamoDB connection

--- database.go
+++ database.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
 )
 
-func MigrateDatabase() {
-	fmt.Println("Migrating database...")
+func InitializeDynamodb() {
+	dynamoDbRegion := os.Getenv("AWS_DYNAMODB_REGION")
+	dynamoDbEndpoint := os.Getenv("AWS_DYNAMODB_ENDPOINT")
+
+	config := &aws.Config{
+		Region:   aws.String(dynamoDbRegion),
+		Endpoint: aws.String(dynamoDbEndpoint),
+	}
+
+	sess := session.Must(session.NewSession(config))
+
+	// TODO: Use this variable
+	svc := dynamodb.New(sess)
 }
--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+)
 
 func main() {
-	MigrateDatabase()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	InitializeDynamodb()
 
 	err, content := GetNewsItemContent("https://www.nytimes.com/2020/09/26/opinion/sunday/trump-cuomo-new-york-revenge.html")
 
