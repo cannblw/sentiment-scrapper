267d2150bcce9a97e8627ad83e45a5b06b11c8ce
cannblw <edgar.95.12@gmail.com>
2020-10-31 16:20:04 +0100

    Create News Item repository

--- repository/news-item.repository.go
+++ repository/news-item.repository.go
@@ -1,1 +1,8 @@
 package repository
+
+type NewsItemRepository interface {
+	Save(item *entity.NewsItem) (*entity.NewsItem, error)
+	FindAll() ([]entity.NewsItem, error)
+	FindById(id string) (*entity.NewsItem, error)
+	Delete(item *entity.NewsItem) error
+}
